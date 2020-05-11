2273a404807423e19c1878f63c2c5700297f93f7
Carlos Augusto <carlosass0.45@gmail.com>
2020-05-11 17:11:27 -0300

    Paginas de autenticação e cadastro funcionando, já está cadastrando e fazendo autenticação na aplicação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    dispatch(signInRequest(email, password));
   }
 
   return (
-    <form onSubmit="">
+    <form onSubmit={handleSubmit}>
       <input
-        type="text"
+        type="email"
         placeholder="Seu e-mail: "
         onChange={(e) => setEmail(e.target.value)}
         value={email}
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Seu nome: "
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <input
+        type="email"
+        placeholder="Seu e-mail:"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        placeholder="Sua senha:"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+
+      <button type="submit">Cadastrar-se</button>
+      <Link to="/">Já possui conta? Faça login!</Link>
+    </form>
+  );
 }
